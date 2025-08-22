609536b48764c9079520788955dbe928f4a6c9b6
chris-henry-holland <chris.henry.holland@gmail.com>
2025-08-22 03:22:14 +0100

    Further experimenting with contents of __init__.py file to get VSCode to regognise import in another module.

--- src/data_structures_and_algorithms/__init__.py
+++ src/data_structures_and_algorithms/__init__.py
@@ -1,1 +1,9 @@
 #! /usr/bin/env python
+
+from data_structures_and_algorithms.addition_chains import *
+
+from array_algorithms import *
+
+from AVL_tree import *
+
+from bipartite_graph_algorithms import *
